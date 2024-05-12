6d5cca725baf8070878550b349e29b09b68c7f56
IRuanzzI <rp824194@gmail.com>
2024-05-12 00:13:29 -0300

    Documetando API

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_employee (name, age, salary) VALUES ('Diniz', 33, 25500);
+INSERT INTO tb_employee (name, age, salary) VALUES ('Luiza', 76, 268000);
+INSERT INTO tb_employee (name, age, salary) VALUES ('Ruan', 54, 560000);
